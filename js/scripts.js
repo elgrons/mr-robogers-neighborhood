0dd24b46617878916816026bd2dfdc1ab3df58d0
Eliot Gronstal <eliot.lauren@gmail.com>
2023-01-22 19:17:20 -0800

    add css styles.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 // // BUSINESS LOGIC
 
+//this function only applies the rules to 1, 2, and 3, but does not account for nubers like 10, 11, 12, etc.//
+
 // function fredBot(input) {
 //   let fredBotArray = []
 //   for (let i = 0; i <= input; i +=1) {
@@ -7,7 +9,6 @@
 //     console.log("array" + fredBotArray[i]);
 //     if (i === 1) {
 //       fredBotArray.push("Beep!");
-//       console.log("1===TRUE");
 //     }
 //     else if (i === 2) {
 //         fredBotArray.push("Boop!");
@@ -19,24 +20,25 @@
 //         fredBotArray.push(i);
 //     }
 //   };
-//   return fredBotArray
+//   return fredBotArray.toString();
 //   }
 
+//this function somehow puts everything as 1(beep)//
   function fredBot(input) {
     let fredBotArray = []
     for (let i = 1; i <= input; i +=1) {
       console.log("index" + i);
       console.log("array" + fredBotArray[i]);
       let stringBot = fredBotArray.toString();
-      if (stringBot.includes("3")) {
+      if (stringBot.includes(3)) {
           fredBotArray.push("Won't you be my neighbor?");
-        console.log("3===TRUE");
+          console.log("3===TRUE");
       }
-      else if (stringBot.includes("2")) {
+      else if (stringBot.includes(2)) {
           fredBotArray.push("Boop!");
       }
-      else if (stringBot.includes("1")) {
-        fredBotArray.push("Beep!");
+      else if (stringBot.includes(1)) {
+          fredBotArray.push("Beep!");
       }
       else {
           fredBotArray.push(i);
@@ -49,14 +51,11 @@
 
 function handleFormSubmission() {
   event.preventDefault();
-  const rogersBotInput = document.getElementById("rogers-bot").value;
-  let fredMcfeelyBot = fredBot(rogersBotInput);
-  document.getElementById("robo-speech").innerText = fredMcfeelyBot;
-
+  const botCount = document.getElementById("input").value;
+  let botSpeak = fredBot(botCount);
+  document.getElementById("input").innerText = botSpeak;
+}
 
 window.addEventListener("load", function() {
   document.querySelector("form#mr-roboger").addEventListener("submit", handleFormSubmission);
 });
-}
-
-// HOW TO SAY DISPLAY FREDBOT- to use innerText I need to turn my FredBot output into a string!
