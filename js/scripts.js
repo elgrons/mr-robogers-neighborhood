4cd8c4e0d2d312f80382fa9f6f6fb1e7a993b6c2
Eliot Gronstal <eliot.lauren@gmail.com>
2023-01-22 20:29:16 -0800

    WIP: site progress needs a meeting with a teacher to go further.

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@
 
 //this function only applies the rules to 1, 2, and 3, but does not account for nubers like 10, 11, 12, etc.//
 
+// let fredBotArray = []
 // function fredBot(input) {
-//   let fredBotArray = []
 //   for (let i = 0; i <= input; i +=1) {
 //     console.log("index" + i);
 //     console.log("array" + fredBotArray[i]);
@@ -24,36 +24,35 @@
 //   }
 
 //this function somehow puts everything as 1(beep)//
-  function fredBot(input) {
-    let fredBotArray = []
-    for (let i = 1; i <= input; i +=1) {
-      console.log("index" + i);
-      console.log("array" + fredBotArray[i]);
-      let stringBot = fredBotArray.toString();
-      if (stringBot.includes(3)) {
-          fredBotArray.push("Won't you be my neighbor?");
-          console.log("3===TRUE");
-      }
-      else if (stringBot.includes(2)) {
-          fredBotArray.push("Boop!");
-      }
-      else if (stringBot.includes(1)) {
-          fredBotArray.push("Beep!");
-      }
-      else {
-          fredBotArray.push(i);
-      }
-    };
-    return fredBotArray.toString();
-  }
+let fredBotArray = []
+function fredBot(input) {1
+  for (let i = 1; i <= input; i +=1) {
+    console.log("index" + i);
+    console.log("array" + fredBotArray[i]);
+    let stringBot = fredBotArray.toString();
+    if (stringBot.includes("3")) {
+        fredBotArray.push("Won't you be my neighbor?");
+    }
+    else if (stringBot.includes("2")) {
+        fredBotArray.push("Boop!");
+    }
+    else if (stringBot.includes("1")) {
+        fredBotArray.push("Beep!");
+    }
+    else {
+        fredBotArray.push(i);
+    }
+  };
+  return fredBotArray.toString();
+}
 
 // USER INTERFACE LOGIC
 
 function handleFormSubmission() {
   event.preventDefault();
   const botCount = document.getElementById("input").value;
-  let botSpeak = fredBot(botCount);
-  document.getElementById("input").innerText = botSpeak;
+  const botSpeak = fredBot(botCount);
+  document.getElementById("number").innerText = botSpeak;
 }
 
 window.addEventListener("load", function() {
